a2cf73fa9092bc441b38593c134a06753232ad6c
Dylan Mashini <dylanmashini123@gmail.com>
2024-06-21 14:45:58 -0400

    Feat: Tangent in Math module

--- src/math.rs
+++ src/math.rs
@@ -18,12 +18,21 @@ pub fn cos(args: Vec<Value>) -> Value {
 }
 
 pub fn sin(args: Vec<Value>) -> Value {
-    let x = args.get(0).expect("sin must have one argument");
+    let x = args.get(0).expect("SIN must have one argument");
     match x {
         Value::Number(n) => return Value::Number(n.sin()),
         _ => return Value::Number(f64::NAN),
     }
 }
+
+pub fn tan(args: Vec<Value>) -> Value {
+    let x = args.get(0).expect("TAN must have one argument");
+    match x {
+        Value::Number(n) => return Value::Number(n.tan()),
+        _ => return Value::Number(f64::NAN),
+    }
+}
+
 
 pub fn get_math_module() -> Value {
     let mut math = Object::new(HashMap::new());
@@ -43,11 +52,18 @@ pub fn get_math_module() -> Value {
         body: FunctionBody::RustFunc(sin),
     });
 
+    let tan = Value::Function(Function {
+        arguments: vec!["x".to_string()],
+        body: FunctionBody::RustFunc(tan),
+    });
+
     math.fields.insert("sqrt".to_string(), sqrt);
     math.fields.insert("cos".to_string(), cos);
     math.fields.insert("sin".to_string(), sin);
+    math.fields.insert("sin".to_string(), tan);
     math.fields
         .insert("PI".to_string(), Value::Number(std::f64::consts::PI));
+
 
     Value::Object(math)
 }
